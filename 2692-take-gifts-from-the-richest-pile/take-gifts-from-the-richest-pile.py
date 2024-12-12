011ce979f474a7a2cb9fb907207de2d31a226631
Khushi Rastogi <158316467+15khushirastogi@users.noreply.github.com>
2024-12-12 23:00:09 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (8.46%) - LeetSync

--- 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.py
+++ 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.py
@@ -1,7 +1,14 @@
 class Solution:
     def pickGifts(self, gifts: List[int], k: int) -> int:
-        for i in range(k):
-            gifts.sort()
-            gifts[-1]=int(pow(gifts[-1],0.5))
-
-        return sum(gifts)
+        nums = [-num for num in gifts]
+        heapify(nums)
+        
+        while k:
+            tmp = math.isqrt(-heappop(nums))
+            heappush(nums, -tmp)
+            k -= 1
+            
+        return -sum(nums)
+        
+       
+        
