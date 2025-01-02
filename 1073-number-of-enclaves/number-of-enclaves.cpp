9653ab099881cd5e084b27595eed5124caf57fae
Khushi Rastogi <158316467+15khushirastogi@users.noreply.github.com>
2025-01-03 02:13:35 +0530

    Time: 5 ms (88.18%) | Memory: 31.5 MB (79.28%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,41 +1,49 @@
 class Solution {
 public:
-    void dfs(int row, int col, vector<vector<int>>& grid, int drow[], int dcol[]) {
-        int m = grid.size();
-        int n = grid[0].size();
-        grid[row][col] = -1; 
-        for (int i = 0; i < 4; i++) {
-            int nrow = row + drow[i];
-            int ncol = col + dcol[i];
-            if (nrow >= 0 && nrow < m && ncol >= 0 && ncol < n && grid[nrow][ncol] == 1) {
-                dfs(nrow, ncol, grid, drow, dcol);
-            }
-        }
-    }
-
     int numEnclaves(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        int drow[] = {-1, 0, +1, 0};
-        int dcol[] = {0, +1, 0, -1};
-
-        for (int i = 0; i < m; i++) {
-            if (grid[i][0] == 1) dfs(i, 0, grid, drow, dcol);       
-            if (grid[i][n-1] == 1) dfs(i, n-1, grid, drow, dcol);   
-        }
-        for (int i = 0; i < n; i++) {
-            if (grid[0][i] == 1) dfs(0, i, grid, drow, dcol);       
-            if (grid[m-1][i] == 1) dfs(m-1, i, grid, drow, dcol);   
-        }
-
-        int count = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1) {
-                    count++;
+        queue<pair<int,int>> q; 
+        int n = grid.size(); 
+        int m = grid[0].size(); 
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        for(int i = 0;i<n;i++) {
+            for(int j = 0;j<m;j++) {
+                if(i == 0 || j == 0 || i == n-1 || j == m-1) {
+                    if(grid[i][j] == 1) {
+                        q.push({i, j}); 
+                        vis[i][j] = 1; 
+                    }
                 }
             }
         }
-        return count;
+        
+        int delrow[] = {-1, 0, +1, 0};
+        int delcol[] = {0, +1, +0, -1}; 
+        
+        while(!q.empty()) {
+            int row = q.front().first; 
+            int col = q.front().second; 
+            q.pop(); 
+        
+            for(int i = 0;i<4;i++) {
+                int nrow = row + delrow[i];
+                int ncol = col + delcol[i]; 
+
+                if(nrow >=0 && nrow <n && ncol >=0 && ncol < m 
+                && vis[nrow][ncol] == 0 && grid[nrow][ncol] == 1) {
+                    q.push({nrow, ncol});
+                    vis[nrow][ncol] = 1; 
+                }
+            }
+            
+        }
+        
+        int cnt = 0;
+        for(int i = 0;i<n;i++) {
+            for(int j = 0;j<m;j++) {
+                if(grid[i][j] == 1 & vis[i][j] == 0) 
+                    cnt++; 
+            }
+        }
+        return cnt; 
     }
 };
