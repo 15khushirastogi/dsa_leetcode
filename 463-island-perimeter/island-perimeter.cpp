f1753ae545491aaa858208279717cf1a793e3949
Khushi Rastogi <158316467+15khushirastogi@users.noreply.github.com>
2025-02-22 23:03:40 +0530

    Time: 5 ms (54.52%) | Memory: 102 MB (68.53%) - LeetSync

--- 463-island-perimeter/island-perimeter.cpp
+++ 463-island-perimeter/island-perimeter.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
 public:
     int islandPerimeter(vector<vector<int>>& grid) {
         int row=grid.size();
@@ -19,7 +19,7 @@ public:
             int r=q.front().first;
             int c=q.front().second;
             q.pop();
-            
+
             for(int i=0;i<4;i++){
                 int newr=r+dirx[i];
                 int newc=c+diry[i];
@@ -29,6 +29,27 @@ public:
                 }
                 if(newr>=0 && newr<row && newc>=0 && newc<col && grid[newr][newc]==0){
                     perimeter++;
+                }
+            }
+        }
+        return perimeter;
+    }
+};
+
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int row = grid.size();
+        int col = grid[0].size();
+        int perimeter = 0;
+        
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (grid[i][j] == 1) {
+                    if (i == 0 || grid[i - 1][j] == 0) perimeter++; 
+                    if (j == 0 || grid[i][j - 1] == 0) perimeter++; 
+                    if (i == row - 1 || grid[i + 1][j] == 0) perimeter++; 
+                    if (j == col - 1 || grid[i][j + 1] == 0) perimeter++; 
                 }
             }
         }
