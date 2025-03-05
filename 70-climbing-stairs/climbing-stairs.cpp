5a418006fab4c1ca56645e9942d39c20bc0c096e
Khushi Rastogi <158316467+15khushirastogi@users.noreply.github.com>
2025-03-05 23:25:10 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (34.56%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,20 +1,24 @@
 class Solution {
 private:
     int ways(int n, vector<int>& dp){
+        // define base cases
         if(n==0){
             return 1;
         }
         if(n<0){
             return 0;
         }
+        //check dp for the already solved subproblems
         if(dp[n]!=-1){
             return dp[n];
         }
+        //store the result in dp
         dp[n]=ways(n-1,dp)+ways(n-2,dp);
         return dp[n];
     }
 public:
     int climbStairs(int n) {
+        //declare and initialise dp
         vector<int>dp(n+1,-1);
         return ways(n,dp);
     }
