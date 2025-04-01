38e3020a265ce71e0fe70f0a54866269c0e379ad
Khushi Rastogi <158316467+15khushirastogi@users.noreply.github.com>
2025-04-02 00:15:38 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (32.81%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,4 +1,5 @@
-class Solution {
+//USING MEMOIZATION
+class Solution1 {
 private:
     int f(int i, int j, vector<vector<int>>&dp){
         if(i==0 && j==0){
@@ -22,5 +23,35 @@ public:
         vector<vector<int>> dp(m, vector<int>(n, -1));
         return f(m-1,n-1,dp);
 
+    }
+};
+
+//USING TABULATION
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        int count=0;
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        dp[0][0]=1;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0){
+                    dp[i][j]=1;
+                }
+                else{
+                    int up=0;
+                    int left=0;
+                    if(i>0){
+                        up=dp[i-1][j];
+                    }
+                    if(j>0){
+                        left=dp[i][j-1]; 
+                    }
+                    dp[i][j]=up+left;
+                }
+                
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
