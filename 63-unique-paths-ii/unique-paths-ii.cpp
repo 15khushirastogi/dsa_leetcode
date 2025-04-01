6a140ba001f4c307156f3f68c3b60381c5e6f794
Khushi Rastogi <158316467+15khushirastogi@users.noreply.github.com>
2025-04-02 00:40:12 +0530

    Time: 4 ms (5.59%) | Memory: 11.9 MB (53.09%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
 private:
     int f(int i, int j, vector<vector<int>>& dp, vector<vector<int>>& obstacleGrid){
         //base conditions
@@ -36,5 +36,33 @@ public:
         }
         vector<vector<int>>dp(m,vector<int>(n,-1));
         return f(m-1,n-1,dp,obstacleGrid);
+    }
+};
+
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int count=0;
+        int m=obstacleGrid.size();
+        int n=obstacleGrid[0].size();
+        if(obstacleGrid[0][0]==1 || obstacleGrid[m-1][n-1]==1){
+            return 0;
+        }
+        vector<vector<int>>dp(m,vector<int>(n,0));
+        dp[0][0]=1;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                int up=0;
+                int left=0;
+                if(obstacleGrid[i][j]==1){
+                    dp[i][j]=0;
+                }
+                else{
+                    if (i > 0) dp[i][j] += dp[i-1][j];
+                    if (j > 0) dp[i][j] += dp[i][j-1];
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
