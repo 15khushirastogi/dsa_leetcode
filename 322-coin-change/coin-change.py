47bbc1d72cf7da9eb7589c9b6826b963398bbf17
Khushi Rastogi <158316467+15khushirastogi@users.noreply.github.com>
2025-04-12 18:47:34 +0530

    Time: 1437 ms (8.00%) | Memory: 21.6 MB (22.89%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,29 +1,25 @@
-from typing import List
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         n=len(coins)
-        dp=[[-1 for _ in range(amount+1)] for _ in range(n)]
-        def f(ind,target):
-            if(ind==0):
-                if(target%coins[ind]==0):
-                    return target//coins[0]
-                else:
-                    return float('inf')
+        dp=[[0 for _ in range(amount+1)] for _ in range(n)]
+        #when we reach the first element
+        for i in range(amount+1):
+            if(i%coins[0]==0):
+                dp[0][i]=i//coins[0]
+            else:
+                dp[0][i]=float('inf')
+        for ind in range(1,n):
+            for target in range(amount+1):
+                nottake=0+dp[ind-1][target]
+                take=float('inf')
+                if(target>=coins[ind]):
+                    take=1+dp[ind][target-coins[ind]]
+                
+                dp[ind][target]=min(take,nottake)
 
-            if(dp[ind][target]!=-1):
-                return dp[ind][target]
-            nottake=0+f(ind-1,target)
-            take=float('inf')
-            if(target>=coins[ind]):
-                take=1+f(ind,target-coins[ind])
-            
-            dp[ind][target]=min(take,nottake)
 
-            return dp[ind][target]
-
-        res=f(n-1,amount)
+        res=dp[n-1][amount]
         if(res!=float('inf')):
             return res
         else:
             return -1
-
