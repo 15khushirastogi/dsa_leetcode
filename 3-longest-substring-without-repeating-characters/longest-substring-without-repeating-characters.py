b4022306715e3775dcdd70d271f6014f740dccc5
Khushi Rastogi <158316467+15khushirastogi@users.noreply.github.com>
2025-04-17 11:19:06 +0530

    Time: 14 ms (84.81%) | Memory: 17.7 MB (82.59%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,22 +1,13 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        max_count=1
-        if(len(s)==0):
-            return 0
-        else:
-            for i in range(0,len(s)-1):
-                l1=[]
-                for j in range(i,len(s)):
-                    if(s[j] in l1):
-                        break
-                    else:
-                        l1.append(s[j])
-                        count=len(l1)
-                if(count>max_count):
-                    max_count=count
-            return max_count
+        hashmap = {}  
+        maxlen = 0
+        i = 0  
 
+        for j in range(len(s)):
+            if s[j] in hashmap and hashmap[s[j]] >= i:
+                i = hashmap[s[j]] + 1  
+            hashmap[s[j]] = j 
+            maxlen = max(maxlen, j - i + 1)  
 
-
-        
-
+        return maxlen
