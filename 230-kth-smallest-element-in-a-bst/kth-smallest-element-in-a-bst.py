c8a26029f2d0e74f4c69bf71b932cbf983cbd349
Khushi Rastogi <158316467+15khushirastogi@users.noreply.github.com>
2025-04-18 14:50:46 +0530

    Time: 4 ms (15.67%) | Memory: 21 MB (73.07%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -4,20 +4,15 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-import heapq
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        heap=[]
+        arr=[]
         def inorder(root):
             if root is None:
                 return
             inorder(root.left)
-            heapq.heappush(heap,root.val)
+            arr.append(root.val)
             inorder(root.right)
         inorder(root)
 
-        ans=0
-        for i in range(k):
-            ans=heapq.heappop(heap)
-
-        return ans
+        return arr[k-1]
