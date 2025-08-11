944b4dadeb07519aac51644c0a8d94d6c902d8d1
Khushi Rastogi <158316467+15khushirastogi@users.noreply.github.com>
2025-08-12 00:19:03 +0530

    Time: 44 ms (48.22%) | Memory: 37.8 MB (28.83%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.py
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.py
@@ -1,20 +1,24 @@
 class Solution:
     def makeConnected(self, n: int, connections: List[List[int]]) -> int:
+        #first check the base possibility condition
         if len(connections)<n-1:
             return -1
-        
+
+        #build adjacency list 
         graph=[[] for _ in range(n)]
         for a,b in connections:
             graph[a].append(b)
             graph[b].append(a)
 
         vis=[0]*n
+
         def dfs(i):
             vis[i]=1
             for nei in graph[i]:
                 if not vis[nei]:
                     dfs(nei)
-
+                    
+        #write the drivers code or the basic logic
         components=0
         for i in range(n):
             if vis[i]==0:
