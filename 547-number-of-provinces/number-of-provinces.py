29371e039872e58b8d4fa5bcbc04c8440eb26542
Khushi Rastogi <158316467+15khushirastogi@users.noreply.github.com>
2025-11-17 11:38:23 +0530

    Time: 11 ms (27.22%) | Memory: 19.1 MB (58.73%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -2,48 +2,21 @@ class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
         n=len(isConnected)
         adj=[[]for _ in range(n)]
-        for u in range(n):
-            for v in range(n):
-                if isConnected[u][v]==1:
-                    adj[u].append(v)
-        parent=[i for i in range(n)]
-        rank=[0]*n
-        def find(x):
-            if parent[x]==x:
-                return x
-            
-            parent[x]=find(parent[x])
-            
-            return parent[x]
-
-        def union(x,y):
-            px=find(x)
-            py=find(y)
-            
-            if px==py:
-                return 
-            if rank[px]>rank[py]:
-                parent[py]=px
-            elif rank[py]>rank[px]:
-                parent[px]=py
-            else:
-                parent[px]=py
-                rank[py]+=1
-
-        for u in range(n):
-            for v in adj[u]:
-                pu=find(u)
-                pv=find(v)
-
-                if pu!=pv:
-                    union(u,v)
-
-        mp={}
         for i in range(n):
-            leader=find(i)
-            if leader in mp:
-                mp[leader]+=1
-            else:
-                mp[leader]=1
+            for j in range(n):
+                if isConnected[i][j]==1:
+                    adj[i].append(j)
+        count=0
+        def dfs(node):
+            vis[node]=1
+            for nei in adj[node]:
+                if vis[nei]==0:
+                    dfs(nei)
+        
+        vis=[0]*n
+        for i in range(n):
+            if vis[i]==0:
+                count+=1
+                dfs(i)
 
-        return len(mp)
+        return count
