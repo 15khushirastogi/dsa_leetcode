b5e5994c7d21238c0c4029be2800c9027a544399
Khushi Rastogi <158316467+15khushirastogi@users.noreply.github.com>
2025-11-17 12:27:00 +0530

    Time: 250 ms (51.26%) | Memory: 20.1 MB (82.24%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,22 +1,30 @@
+from collections import deque
+
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        m=len(grid)
-        n=len(grid[0])
-        cnt=0
-        vis=[[0 for _ in range(n)] for _ in range(m)]
-        def dfs(i,j):
-            vis[i][j]=1
-            dir=[[0,1],[1,0],[0,-1],[-1,0]]
-            for dr in dir:
-                newr=i+dr[0]
-                newc=j+dr[1]
-                if newr>=0 and newc>=0 and newr<m and newc<n and grid[newr][newc]=='1' and vis[newr][newc]==0:
-                    dfs(newr,newc)
+        m, n = len(grid), len(grid[0])
+        vis = [[0] * n for _ in range(m)]
 
+        def bfs(r, c):
+            q = deque()
+            q.append((r, c))
+            vis[r][c] = 1
+
+            directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+
+            while q:
+                row, col = q.popleft()
+                for dr, dc in directions:
+                    nr, nc = row + dr, col + dc
+                    if 0 <= nr < m and 0 <= nc < n and vis[nr][nc] == 0 and grid[nr][nc] == "1":
+                        vis[nr][nc] = 1
+                        q.append((nr, nc))
+
+        count = 0
         for i in range(m):
             for j in range(n):
-                if grid[i][j]=='1' and vis[i][j]==0:
-                    cnt+=1
-                    dfs(i,j)
+                if grid[i][j] == "1" and vis[i][j] == 0:
+                    bfs(i, j)
+                    count += 1
 
-        return cnt
+        return count
